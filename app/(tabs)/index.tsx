3d722ae5ecab566e3952739d25ea3d60306ceb64
violessi <108144748+violessi@users.noreply.github.com>
2024-11-17 23:58:08 +0800

    chore: remove initial files

--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Image, StyleSheet, Text, SafeAreaView } from "react-native";
-
-export default function HomeScreen() {
-  return (
-    <SafeAreaView className="flex-1 items-center justify-center">
-      <Text className="text-pink-500">Testing!</Text>
-    </SafeAreaView>
-  );
-}
