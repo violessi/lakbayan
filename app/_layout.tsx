4924269cfe5888a0fae622f7f38ec763730a4bb3
violessi <108144748+violessi@users.noreply.github.com>
2024-11-17 23:58:37 +0800

    chore: setup react paper native

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
+import { PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   return (
+    <PaperProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       </Stack>
+    </PaperProvider>
   );
 }
