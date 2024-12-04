f1432ea8a2ec3ba7b9531dd1385bb57a04cad29a
inunice <ammanguan@up.edu.ph>
2024-12-04 20:47:15 +0800

    feat: home page styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet, SafeAreaView } from "react-native";
 import Mapbox, { MapView, Camera, LocationPuck } from "@rnmapbox/maps";
 import { MAPBOX_ACCESS_TOKEN } from "../../utils/mapbox-config";
 
@@ -6,9 +7,17 @@ Mapbox.setAccessToken(MAPBOX_ACCESS_TOKEN);
 
 export default function Index() {
   return (
-    <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/streets-v12">
-      <Camera followZoomLevel={13} followUserLocation />
-      <LocationPuck />
-    </MapView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View className="h-20 bg-primary px-5 py-5">
+        <Text className="text-white text-lg font-bold">Hi, user!</Text>
+      </View>
+      <MapView
+        style={{ flex: 1 }}
+        styleURL="mapbox://styles/mapbox/streets-v12"
+      >
+        <Camera followZoomLevel={13} followUserLocation />
+        <LocationPuck />
+      </MapView>
+    </SafeAreaView>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#7F55D9",
+      },
+    },
   },
   plugins: [],
 };
