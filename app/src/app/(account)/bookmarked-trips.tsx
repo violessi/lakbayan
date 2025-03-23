f7c00ddf66fa9974baa3cd0d08c63d7d2d5e7974
inunice <ammanguan@up.edu.ph>
2025-03-24 07:17:09 +0800

    feat: display bookmarked trips

--- app/src/app/(account)/bookmarked-trips.tsx
+++ app/src/app/(account)/bookmarked-trips.tsx
@@ -1,14 +1,52 @@
-import React from "react";
-import { SafeAreaView, View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, View, FlatList, ActivityIndicator, Text } from "react-native";
+
+import { useSession } from "@contexts/SessionContext";
+import { getBookmarks } from "@services/socials-service";
+import { useTripData } from "@hooks/use-trip-data";
 
 import Header from "@components/ui/Header";
+import TripPreview from "@components/ui/TripPreview";
 
 export default function BookmarkedTrips() {
+  const { userId } = useSession();
+  const { tripData, segmentData, loading: tripsLoading } = useTripData();
+  const [bookmarkedTrips, setBookmarkedTrips] = useState<Trip[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchBookmarks = async () => {
+      if (!userId) return;
+
+      setLoading(true);
+      const tripIds = await getBookmarks(userId);
+
+      const filteredTrips = tripData.filter((trip) => tripIds.includes(trip.id));
+      setBookmarkedTrips(filteredTrips);
+
+      setLoading(false);
+    };
+
+    if (!tripsLoading) {
+      fetchBookmarks();
+    }
+  }, [userId, tripData, tripsLoading]);
+
   return (
     <SafeAreaView className="flex-1">
       <Header title="Bookmarked Trips" />
       <View className="flex-1 p-4">
-        <Text>Test</Text>
+        {loading || tripsLoading ? (
+          <ActivityIndicator size="small" />
+        ) : bookmarkedTrips.length === 0 ? (
+          <Text>No bookmarked trips</Text>
+        ) : (
+          <FlatList
+            data={bookmarkedTrips}
+            keyExtractor={(trip) => trip.id}
+            renderItem={({ item }) => <TripPreview trip={item} segments={segmentData[item.id] || []} />}
+          />
+        )}
       </View>
     </SafeAreaView>
   );
--- app/src/app/(account)/submitted-trips.tsx
+++ app/src/app/(account)/submitted-trips.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { SafeAreaView, View, Text } from "react-native";
 
+import { useSession } from "@contexts/SessionContext";
+
 import Header from "@components/ui/Header";
 
 export default function SubmittedTrips() {
+  const { userId } = useSession();
+
   return (
     <SafeAreaView className="flex-1">
       <Header title="Submitted Trips" />
