7b81b733a2b1884e161f8d963886d3175911951f
inunice <ammanguan@up.edu.ph>
2025-03-25 15:37:11 +0800

    feat: tab icons

--- app/src/app/(tabs)/_layout.tsx
+++ app/src/app/(tabs)/_layout.tsx
@@ -1,30 +1,52 @@
 import { Tabs, Redirect } from "expo-router";
+import { Image, ImageSourcePropType, StyleSheet } from "react-native";
+
 import { useSession } from "@contexts/SessionContext";
+
+const homeIcon = require("@assets/tab-home.png");
+const contributeIcon = require("@assets/tab-contribute.png");
+const accountIcon = require("@assets/tab-account.png");
+
+const TabIcon = ({ source, size, focused }: { source: ImageSourcePropType; size: number; focused: boolean }) => (
+  <Image
+    source={source}
+    style={[styles.icon, { width: size, height: size, tintColor: focused ? "#7F55D9" : "gray" }]}
+    resizeMode="contain"
+  />
+);
 
 export default function TabLayout() {
   const { user } = useSession();
   if (!user) return <Redirect href="/(auth)/log-in" />;
 
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#7F55D9",
+        headerShown: false,
+        tabBarStyle: {
+          paddingTop: 4,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
-        options={{
-          title: "Home",
-        }}
+        options={{ title: "Home", tabBarIcon: (props) => <TabIcon source={homeIcon} {...props} /> }}
       />
       <Tabs.Screen
         name="contribute"
-        options={{
-          title: "Contribute",
-        }}
+        options={{ title: "Contribute", tabBarIcon: (props) => <TabIcon source={contributeIcon} {...props} /> }}
       />
       <Tabs.Screen
         name="account"
-        options={{
-          title: "Account",
-        }}
+        options={{ title: "Account", tabBarIcon: (props) => <TabIcon source={accountIcon} {...props} /> }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    tintColor: "gray",
+  },
+});
