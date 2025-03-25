a17e6c04dd81157a93872888694d5cb7f4467cf3
Jeric <62868605+JericN@users.noreply.github.com>
2025-03-25 16:26:51 +0800

    fix: prevent search bar clicks from reaching map
    Co-Authored-By: violessi <108144748+violessi@users.noreply.github.com>

--- app/src/components/StartEndSearchBar.tsx
+++ app/src/components/StartEndSearchBar.tsx
@@ -65,7 +65,7 @@ export default function StartEndSearch({
   };
 
   return (
-    <View className="absolute top-0 left-0 z-40 flex flex-col bg-white rounded-[8]" style={{ width: "100%" }}>
+    <View>
       <View className="absolute top-0 left-0 z-50 m-5 flex-row items-center bg-white rounded-[8]">
         <TextInput
           placeholder={defaultStart || "Starting location"}
