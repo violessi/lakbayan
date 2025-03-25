c6761e78807c72888dca222a7d7db707e08cb5ab
inunice <ammanguan@up.edu.ph>
2025-03-25 23:30:07 +0800

    feat: slightly better onboarding

--- app/src/app/(auth)/onboarding.tsx
+++ app/src/app/(auth)/onboarding.tsx
@@ -4,11 +4,8 @@ import { Text, View, SafeAreaView, TouchableOpacity } from "react-native";
 export default function Onboarding() {
   return (
     <SafeAreaView className="flex h-full items-center justify-between">
-      <TouchableOpacity
-        className="bg-blue-500 p-2 rounded-md justify-end"
-        onPress={() => router.replace("/(auth)/log-in")}
-      >
-        <Text>Skip</Text>
+      <TouchableOpacity className="px-5 py-3 rounded-lg justify-end" onPress={() => router.replace("/(auth)/log-in")}>
+        <Text className="text-primary font-bold border-b border-primary">Skip</Text>
       </TouchableOpacity>
       <View className="flex h-full items-center justify-center">
         <Text className="text-pink-500">Onboarding!</Text>
