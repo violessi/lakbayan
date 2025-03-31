33120f42e6afd03f8bfa6c97431a4ed3ee3c0373
inunice <ammanguan@up.edu.ph>
2025-03-31 17:56:12 +0800

    fix: view of submitted trips

--- app/src/app/(account)/submitted-trips.tsx
+++ app/src/app/(account)/submitted-trips.tsx
@@ -10,39 +10,38 @@ import {
 import { useRouter } from "expo-router";
 
 import { useSession } from "@contexts/SessionContext";
-import { useTripData } from "@hooks/use-trip-data";
+import { useUserTrips } from "@hooks/use-trip-data";
 
 import Header from "@components/ui/Header";
 import TripPreview from "@components/ui/TripPreview";
 
 export default function SubmittedTrips() {
   const { user } = useSession();
-  const { tripData, segmentData, loading: tripsLoading } = useTripData();
-
-  const [submittedTrips, setSubmittedTrips] = useState<Trip[]>([]);
-  const [loading, setLoading] = useState(true);
-
   const router = useRouter();
+
+  const { userTrips, loading: tripsLoading } = useUserTrips(user?.id || "");
+  const [loading, setLoading] = useState(true);
+  const [submittedTrips, setSubmittedTrips] = useState<TripSearch[]>([]);
 
   useEffect(() => {
     if (!user || tripsLoading) return;
 
     setLoading(true);
 
-    // Filter trips where the user is the contributor
-    const userTrips = tripData.filter((trip) => trip.contributorId === user.id);
-    setSubmittedTrips(userTrips);
+    const preparedTrips: TripSearch[] = userTrips.map((trip) => ({
+      ...trip,
+      preSegment: null,
+      postSegment: null,
+    }));
 
+    setSubmittedTrips(preparedTrips);
     setLoading(false);
-  }, [user, tripData, tripsLoading]);
+  }, [user, userTrips, tripsLoading]);
 
-  function handleTripPress(trip: Trip) {
+  function handleTripPress(trip: TripSearch) {
     router.push({
       pathname: "/(search)/3-trip-overview",
-      params: {
-        trip: JSON.stringify(trip),
-        segments: JSON.stringify(segmentData[trip.id] || []),
-      },
+      params: { tripData: JSON.stringify(trip) },
     });
   }
 
@@ -53,14 +52,16 @@ export default function SubmittedTrips() {
         {loading || tripsLoading ? (
           <ActivityIndicator size="small" />
         ) : submittedTrips.length === 0 ? (
-          <Text>No submitted trips</Text>
+          <View className="flex-1 justify-center items-center">
+            <Text>No submitted trips</Text>
+          </View>
         ) : (
           <FlatList
             data={submittedTrips}
             keyExtractor={(trip) => trip.id}
             renderItem={({ item }) => (
               <TouchableOpacity onPress={() => handleTripPress(item)}>
-                <TripPreview trip={item} segments={segmentData[item.id] || []} />
+                <TripPreview trip={item} />
               </TouchableOpacity>
             )}
           />
